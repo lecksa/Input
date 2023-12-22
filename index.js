a6dee8410f91ed085e2cc9ca99823dafe4827633
lecksa <dolabesipa@gmail.com>
2023-12-22 22:13:18 +0500

    fixed html and js

--- index.js
+++ index.js
@@ -13,23 +13,59 @@
 // }
 
 
-let inp = document.querySelectorAll('.item_2')
-let h1 = document.querySelectorAll('.item_3')
-let img = document.querySelectorAll('img')
-let but = document.querySelector('button')
-let error = document.querySelector('.error')
-let success = document.querySelector('.success')
+let form = document.forms.wrap
+let inputs = document.querySelectorAll('.success input')
+let simps = document.querySelectorAll('.sim input')
 
-inp.forEach((input, idx) => {
-    let inp_val = input[idx].value
-    if (inp_val === "") {
-        inp_val.classList.toggle('active_2')
-        h1.classList.toggle('active_3')
-        img.src = "./img/error warning.jpg"
-     } 
-    //else {
-    //     input.classList.remove('active_2')
-    //     h1.classList.remove('active_3')
-    //     but.type = 'submit'
-    // }
-})
+let rights = document.querySelector('.Right')
+let wrongs = document.querySelector('.Wrong')
+
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    
+    let errors = 0
+    
+    let successes = 0
+    
+    simps.forEach(sim => {
+        if(sim.value.length > 0){
+            successes++
+        }
+    })
+
+    inputs.forEach(inp => {
+        if (inp.value.length === 0) {
+            inp.parentElement.classList.remove('success')
+            inp.parentElement.classList.add('error')
+            errors++
+        } else {
+            inp.parentElement.classList.remove('error')
+            inp.parentElement.classList.add('success')
+            successes++
+        }
+    })
+    
+    if(errors > 0){
+        alert('Вы должны заполнить все обязательные пункты!')
+        wrongs.innerHTML = errors
+        rights.innerHTML = successes
+    }else{
+        rights.innerHTML = successes
+        wrongs.innerHTML = errors
+        button()
+    }
+}
+
+function button() {
+    let person = {}
+
+    let data = new FormData(form)
+
+    data.forEach((value, key) => {
+        person[key] = value
+    })
+
+    console.log(person);
+}
